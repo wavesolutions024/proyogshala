0b2a3cc02b2f70d3b938174630b8bc33b7d87548
Ketan Dudka <ketandudka566123@gmail.com>
2025-09-11 14:02:40 +0530

    push

---
+++ src/comp/header/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import "./Header.scss"
+const Header = () => {
+
+    const navs 
+  return (
+    <>
+      <div class="header_parent parent">
+        <div class="header_cont cont">
+            <div class="navsection">
+
+            </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Header
