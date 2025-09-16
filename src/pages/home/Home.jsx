69197addd3fe90dc29b224983965c6761662019c
Ketan Dudka <ketandudka566123@gmail.com>
2025-09-16 16:51:47 +0530

    push

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,47 @@
-import React from 'react'
-import Herosection from '../../comp/herosection/Herosection'
-
+import React from "react";
+import Herosection from "../../comp/herosection/Herosection";
+import "./Home.scss";
 const Home = () => {
   return (
     <>
-      <Herosection/>
-    </>
-  )
-}
+      <Herosection />
+      <div class="about_section_parent parent">
+        <div class="about_section_cont cont">
+          <div class="left ">
+            <div class="floral bg-img-contain">
 
-export default Home
+            </div>
+
+            <img src="" alt=""/>
+          </div>
+          <div class="right">
+            <div class="section_indi">About Us</div>
+            <h1>STAY FIT AND HEALTHY WITH PLATES</h1>
+
+            <div class="description">
+              <div class="top">
+                <div class="tag">01</div>
+                <p>
+                  Msenenatis iaculis. Donec a mi enim. Morbi euismod vel dolor
+                  vitae congue. Proin vel dui semper, bibendum leo in,pellentesque magna. 
+                </p>
+              </div>
+              <p>
+                Duis convallis sodales magna. Fusce eu nisl
+                interdum. Msenenatis iaculis. Donec a mi enim. Morbi euismod vel
+                dolor vitae congue. Proin vel dui semper, bibendum leo in,
+                Msenenatis iaculis. Donec a mi enim. Morbi euismod vel dolor
+                vitae congue. Proin vel dui semper, bibendum leo in,
+              </p>
+            </div>
+            <div class="btn">
+              Read More
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
