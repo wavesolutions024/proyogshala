92bbfbb019932b5122bbb8a35061a6a3c77ee241
Ketan Dudka <ketandudka566123@gmail.com>
2025-09-16 18:47:53 +0530

    push

--- src/comp/header/Header.jsx
+++ src/comp/header/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./Header.scss";
 import { Link, useLocation } from "react-router-dom";
 import { IoMdCall } from "react-icons/io";
@@ -6,6 +6,7 @@ import { RxCross2 } from "react-icons/rx";
 const Header = () => {
   const location = useLocation();
   const [active, setActive] = useState(false);
+  const [scrollHeader, setScrollHeader] = useState(false);
   const navs = [
     {
       name: "Home",
@@ -32,9 +33,26 @@ const Header = () => {
       link: "/contact",
     },
   ];
+
+  useEffect(() => {
+    const scrollHandler = () => {
+      if (window.scrollY >= 10) {
+        setScrollHeader(true);
+      } else {
+        setScrollHeader(false);
+      }
+    };
+
+    window.addEventListener("scroll" , scrollHandler);
+
+
+    return ()=>{
+      window.removeEventListener("scroll" , scrollHandler);
+    }
+  }, []);
   return (
     <>
-      <div class="header_parent parent">
+      <div class={scrollHeader ? "header_parent parent active" : "header_parent parent"}>
         <div class="header_cont cont">
           <div class="logo">
             <div class="logo_imag"></div>
@@ -52,10 +70,10 @@ const Header = () => {
           </div>
           <div class="contact_section">
             <a href="" class="btn">
-                <div class="call_icon">
-              <IoMdCall />
-            </div>
-            <span href="">+91 1234567890</span>
+              <div class="call_icon">
+                <IoMdCall />
+              </div>
+              <span href="">+91 1234567890</span>
             </a>
             <div class="hamburger">
               <div class="open" onClick={() => setActive(true)}>
@@ -65,28 +83,27 @@ const Header = () => {
               </div>
             </div>
           </div>
-         
-            <div class={active ? "mob_nav active" : "mob_nav"}>
-              <div class="top">
-                <div class="logo"></div>
-                <div class="cross" onClick={() => setActive(false)}>
-                  <RxCross2 />
-                </div>
-              </div>
-              <div class="bottom">
-                {navs.map((item, index) => (
-                  <Link
-                    onClick={() => setActive(false)}
-                    className={location.pathname === item.link ? "active" : ""}
-                    key={index}
-                    to={item.link}
-                  >
-                    {item.name}
-                  </Link>
-                ))}
+
+          <div class={active ? "mob_nav active" : "mob_nav"}>
+            <div class="top">
+              <div class="logo"></div>
+              <div class="cross" onClick={() => setActive(false)}>
+                <RxCross2 />
               </div>
             </div>
-        
+            <div class="bottom">
+              {navs.map((item, index) => (
+                <Link
+                  onClick={() => setActive(false)}
+                  className={location.pathname === item.link ? "active" : ""}
+                  key={index}
+                  to={item.link}
+                >
+                  {item.name}
+                </Link>
+              ))}
+            </div>
+          </div>
         </div>
       </div>
     </>
