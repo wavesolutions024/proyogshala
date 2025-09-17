4a5f3afa0d3a28f869d26dc3e5f0283da757b760
ketanrd26 <ketandudka566123@gmail.com>
2025-09-17 23:16:29 +0530

    push

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,7 +3,27 @@ import Herosection from "../../comp/herosection/Herosection";
 import "./Home.scss";
 import image1 from "../../assets/dummy_icon.png";
 import Ustrasana from "../../assets/yoga_types/Ustrasana.png";
+import ReactPlayer from "react-player";
+import video1 from "../../../public/videos/testimoneal/test_1.webm";
 const Home = () => {
+  const testimoneal = [
+    {
+      video: video1,
+      name: "Shruti Nagar",
+    },
+    {
+      video: video1,
+      name: "Nidhi saha",
+    },
+    {
+      video: video1,
+      name: "Vishal sharma",
+    },
+    {
+      video: video1,
+      name: "Sumit wakade",
+    },
+  ];
   return (
     <>
       <Herosection />
@@ -43,7 +63,9 @@ const Home = () => {
       <div class="yoga_types_parent parent">
         <div class="yoga_types_cont cont">
           <div class="left">
-            <h3>YOGA TYPES / <span>YOGA POSITIONS</span> </h3>
+            <h3>
+              YOGA TYPES / <span>YOGA POSITIONS</span>{" "}
+            </h3>
             <div class="types">
               <div class="left">
                 <div class="type">
@@ -137,6 +159,40 @@ const Home = () => {
         </div>
 
         <div class="left_panel bg-img-cover"></div>
+      </div>
+
+      <div class="video_section parent bg-img-cover">
+       
+        <p>Lorem ipsum dolor sit amet.</p>
+        <h1>The Natural Way To get your mind & body together</h1>
+      </div>
+
+      <div class="testimoneal_parent parent">
+        <div class="testimoneal_cont cont">
+          <h1>Testimonials</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis
+            nulla pariatur, ea esse nemo officiis dignissimos, temporibus iusto
+            tenetur similique iure non illum tempora id cupiditate fugit magni
+            sapiente! Necessitatibus.
+          </p>
+
+          <div class="client-video">
+            {testimoneal.map((item, index) => (
+              <div class="video" key={index}>
+                <div class="video-section">
+                  <ReactPlayer
+                    src={item.video}
+                    width="100%"
+                    height="100%"
+                    controls
+                  />
+                </div>
+                <h2 class="client_name">{item.name}</h2>
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
     </>
   );
