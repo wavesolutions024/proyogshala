218f110980d1a86ab3175869dd633a72b8ab00ad
rishabh <rishabhkhade95@gmail.com>
2025-09-20 17:35:54 +0530

    pp

--- src/pages/shankanad/Shankanad.jsx
+++ src/pages/shankanad/Shankanad.jsx
@@ -1,18 +1,20 @@
-import React from 'react'
-import "./Shankanad.scss"
-import PageTop from '../../comp/pagetop/PageTop'
+import React from "react";
+import "./Shankanad.scss";
+import PageTop from "../../comp/pagetop/PageTop";
 
 const Shankanad = () => {
   return (
     <>
-    <PageTop title="Shankanad" />
-      <div className="parent">
-        <div className="cont">
+      <PageTop title="Shankanad" />
+      <div className="parent shank-parent">
+        <div className="cont shank-cont">
+          <div className="bg-shank bg-img-cover">
             
+          </div>
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Shankanad
+export default Shankanad;
